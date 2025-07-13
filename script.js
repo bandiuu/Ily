542bb24b135e8ac30c17c1e781e4b0c6308977dd
bandiuu <duranjarabill@gmail.com>
2025-07-13 13:17:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ document.getElementById("data").innerHTML = data.toUpperCase();
 
 const yesBtn = document.querySelector('#yesBtn');
 yesBtn.addEventListener('click', function(){
-    alert('Te amo como no tienes idea😘💕');
+    alert('Mi amor,
+Todavía me cuesta creer que, en medio de una pantalla, entre partidas, risas, enojos y audios eternos, apareciste tú. Tan distinta, tan única, tan tú. Y hoy, después de todo lo que fuimos construyendo de a poco, puedo decir con el corazón lleno que ahora eres mi novia. Qué fuerte se siente eso… pero qué bonito.
+No sabés cuánto me alegra que hayas llegado a mi vida. Tus enojos espontáneos, tus momentos tiernos, tus bromas, tus silencios… todo eso que te hace real. No eres perfecta, y tampoco lo busco. Pero tienes eso que no se encuentra fácil: una forma de llegar, de quedarte y de tocar el alma sin pedir permiso.
+Me gusta escucharte reír, incluso cuando dices que no quieres reírte. Me gusta cómo me hablás cuando estás seria y también cuando te sueltas. Me gusta cómo eres conmigo, pero más aún, cómo me hacés sentir: tranquilo, querido y elegido.
+Sé que no es fácil. Estamos lejos, a veces hay malentendidos, a veces el orgullo choca más de la cuenta… pero lo que siento por ti es más fuerte que cualquier distancia. Porque tú no estás solo en mi pantalla, estás en mis pensamientos, en mis días, en mis planes.
+Y ahora que eres mi novia, quiero que lo sepas con todas sus letras: te elijo, te valoro, y quiero cuidar esto que estamos formando. Quiero darte motivos para sonreír, para confiar, para soñar conmigo, incluso cuando no me tengas al lado.
+Gracias por elegirme, por confiar en mí, por no soltar cuando todo parecía complicado.
+Gracias por ser tú.
+Y si alguna vez dudás de cuánto te amo, acuerdate de esto: aunque estemos lejos, te siento cerca. Aunque el mundo sea una mrd, conmigo siempre vas a tener un lugar tranquilo para descansar el alma.
+Te quiero, te pienso, y ahora más que nunca, te llevo conmigo.
+Bill 💗');
     location.href = 'https://youtu.be/VBiNZcH27Y8?si=pXaOCri64taBQ2el';
 });
 
